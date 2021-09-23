3fc2bce3cbc14a4d9aa7b8963ff55dfa8a2bcc4a
alec <kaltschmidalec@gmail.com>
2021-09-22 17:26:22 -0700

    commit tree 7d3375652235df1dd946c0b3f0b64b6dc9dab6a9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :clickbait?
+
+    # CLICKBAIT_STRINGS = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? {|string| string.match title}
+            errors.add(:title, "this is clickbait")
+        end
+    end
 end
